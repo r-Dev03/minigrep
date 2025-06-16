3f8cc10df108d15766614d489caa32de1262e2b2
Ribbal Hussain <ribbalh0@gmail.com>
2025-06-16 16:43:51 -0500

    Slight refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,32 @@ use std::fs;
 use std::fs::read_to_string;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  let query = &args[1];
-  let fiLe_path = &args[2];
-  
+    let config = Config::new(&args);
 
-  println!("Searching for {query}");
-  println!("In file {fiLe_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
 
 
-  let contents = fs::read_to_string(fiLe_path)
-      .expect("Should have been able to read the file");
-  
-  println!("With text:\n{contents}");
+impl Config {
+  fn new(args: &[String]) -> Config {
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+
+    Config { query, file_path }
+
+  }
 }
